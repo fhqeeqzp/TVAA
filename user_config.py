54f2ce4dd374e24c3e7c9663128e4a19ed3170dc
网络刀客 <49681129+fhqeeqzp@users.noreply.github.com>
2024-08-20 11:52:46 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,11 +1,18 @@
 source_file = "user_demo.txt"
 final_file = "user_result.txt"
 favorite_list = [
-    "广东珠江",
-    "广东影视",
-    "岭南戏曲",
-    "大湾区卫视",
-]
+        "CCTV1-综合", "CCTV2", "CCTV3", "CCTV4", "CCTV5", "CCTV6", "CCTV7", "CCTV8",
+        "CCTV9", "CCTV10", "CCTV11", "CCTV12", "CCTV13", "CCTV14", "CCTV15", "CCTV16",
+        "CCTV17", "CCTV5+", "CCTV4K", "CCTV8K", "CGTN记录", "CCTV风云剧场", "CCTV风云音乐", "CCTV风云足球",
+        "CCTV第一", "CCTV怀旧", "CCTV央视台球", "CCTV央视文化", "CCTV兵器", "CCTV卫生", "CCTV世界",
+        "CCTV高尔夫", "CETV1", "CETV2", "CETV3", "CETV4", "北京卫视", "天津卫视", "河北卫视", "山西卫视", "内蒙古卫视", "辽宁卫视", "吉林卫视", "黑龙江卫视",
+        "东方卫视", "江苏卫视", "浙江卫视", "安徽卫视", "江西卫视", "山东卫视", "山东体育", "河南卫视",
+        "湖北卫视", "湖南卫视", "广东卫视", "广西卫视", "重庆卫视", "四川卫视", "贵州卫视", "云南卫视",
+        "厦门卫视", "西藏卫视", "陕西卫视", "甘肃卫视", "青海卫视", "宁夏卫视", "新疆卫视",
+        "CHC影迷", "CHC家庭", "CHC动作", "凤凰中文", "凤凰卫视", "香港卫视","凤凰资讯", "凤凰香港", "东森电影", "东森新闻", "东森超视", "翡翠", "TVBS亚洲",
+        "TVBS新闻", "本港台", "纬来体育", "J2", "TVBS欢乐","纬来电影", "纬来综合", "无线新闻",
+        "无线财经", "辽宁北方", "辽宁公共", "辽宁体育", "辽宁影视剧", "辽宁都市", "辽宁生活", "辽宁经济", "辽宁教育"
+    ]
 open_online_search = True
 favorite_page_num = 5
 default_page_num = 3
@@ -19,9 +26,10 @@ domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = False
 subscribe_urls = [
-    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/fmml_ipv6.txt",
+    "https://m3u.ibert.me/txt/ycl_iptv.txt",
     "https://m3u.ibert.me/txt/o_cn.txt",
-    "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://m3u.ibert.me/txt/o_s_cn.txt",
 ]
 open_multicast = True
 region_list = ["all"]
